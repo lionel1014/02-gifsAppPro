e81665d1b7e29516badb265572cb8122fa2a00c3
Nick <satur.1512@gmail.com>
2022-01-09 19:49:59 -0300

    Modulación de la aplicación

---
+++ src/app/gifs/buscar-gif/buscar-gif.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buscar-gif',
+  templateUrl: './buscar-gif.component.html',
+  styles: [
+  ]
+})
+export class BuscarGifComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/gifs/resultado-gif/resultado-gif.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resultado-gif',
+  templateUrl: './resultado-gif.component.html',
+  styles: [
+  ]
+})
+export class ResultadoGifComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

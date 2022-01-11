80ac263891f68136ee5b37a8bd703375953e1edb
Nick <satur.1512@gmail.com>
2022-01-11 19:32:12 -0300

    carga imagenes desde el sidebar y se establecio el HttpParams

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { SearchGifsResponse, Gif } from '../interfaces/gifs.interface';
 
@@ -8,7 +8,8 @@ import { SearchGifsResponse, Gif } from '../interfaces/gifs.interface';
 export class GifsService {
 
   private apiKey     :string = 'l9PlLu4Mo722eeo9TEsYuO13aSACclAM';
-  private url        :string = `https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}`;
+  private urlGif        :string = `https://api.giphy.com/v1/gifs`
+  // /search?api_key=${this.apiKey}`;
   private _historial :string[] = [];
 
   public resultados : Gif[] = [];
@@ -17,8 +18,17 @@ export class GifsService {
     return [...this._historial];
   };
 
-  constructor(private http:HttpClient){}
+  constructor(private http:HttpClient){
 
+    if (localStorage.getItem('historial')) {
+      this._historial = JSON.parse( localStorage.getItem('historial')! );
+    };
+
+    this.resultados = JSON.parse( localStorage.getItem('resultados')! ) || [];
+    //* otra forma de preguntar
+  }
+
+  //* Metodo para volver a cargar la información
   buscarGif = (query: string = '') =>{
 
     query = query.trim().toLocaleLowerCase();
@@ -30,12 +40,22 @@ export class GifsService {
       // if (this._historial.length > 10) {
       //   this._historial.pop();
       // };
+
+      localStorage.setItem('historial', JSON.stringify(this._historial))
+
     };
 
-    this.http.get<SearchGifsResponse>(`${this.url}&q=${query}&limit=10`)
+    //! tienen que ser los mismos parametros que requiere el API, revisar el POSTMAN
+    const params = new HttpParams()
+          .set('api_key',this.apiKey)
+          .set('limit', '10')
+          .set('q',query);
+
+    this.http.get<SearchGifsResponse>(`${this.urlGif}/search`,{params})
       .subscribe((response) => {
         console.log(response.data);
         this.resultados = response.data;
+        localStorage.setItem('resultados',JSON.stringify(this.resultados));
       });
 
   };
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -13,4 +13,8 @@ export class SidebarComponent {
     return this.gifsService.historial;
   };
 
+  buscar = ( termino: string ) => {
+    this.gifsService.buscarGif(termino);
+  };
+
 }
